e5b2ff27539b9c08ef3d8c12c6577cc1200625de
Joshua Webb <bbew.auhsoj+git@gmail.com>
2019-02-10 21:23:38 +1100

    Release 0.1.0

---
+++ precache-manifest.888bc48ea452bca73ef01c7225651e98.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "66cb629fe1b1d5823ad1",
+    "url": "./static/js/main.66cb629f.chunk.js"
+  },
+  {
+    "revision": "4a8bc7be5a67403ce1a3",
+    "url": "./static/js/1.4a8bc7be.chunk.js"
+  },
+  {
+    "revision": "66cb629fe1b1d5823ad1",
+    "url": "./static/css/main.944732bd.chunk.css"
+  },
+  {
+    "revision": "66751521e821aa0a91684d9bcf4f4285",
+    "url": "./index.html"
+  }
+];
